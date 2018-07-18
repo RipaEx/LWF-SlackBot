dda256faa4d1c46d37d2df65634c38ad7913cc4e
DEADP0OL <33796817+DEADP0OL@users.noreply.github.com>
2018-07-18 14:59:27 -0500

    Update for RIPA

--- functions.py
+++ functions.py
@@ -20,9 +20,7 @@ def getconfigs(file):
     channelnames = configs.get("channels")
     usernames = configs.get("users")
     numdelegates = configs.get("numdelegates")
-    blockrewards = configs.get("blockrewards")
-    blockspermin = configs.get("blockspermin")
-    return apitoken,url,backup,port,blockinterval,minmissedblocks,channelnames,usernames,numdelegates,blockrewards,blockspermin
+    return apitoken,url,backup,port,blockinterval,minmissedblocks,channelnames,usernames,numdelegates
 
 def cleanurl(url,port):
     cleanitems=['https://','http://','/',':'+port]
@@ -48,25 +46,32 @@ def getpeers(url):
     peers = pd.DataFrame(requests.get(url+'api/peers').json()['peers'])
     return peers
 
-def getstatus(url,backup,tol=1):
-    #gets current height from the list of backup nodes
+def getstatus(url,backup,port,tol=1):
+    """gets current height from the list of backup nodes"""
     backupheights={}
     for i in backup:
         try:
-            backupheights[i]=getheight(i)
+            backupheights[cleanurl(i,port)]='{:,.0f}'.format(getheight(i))
         except:
-            backupheights[i]='not available'
+            backupheights[cleanurl(i,port)]='not available'
     try:
         peers=getpeers(url)
         total=len(peers)
-        peers=peers[peers['state']==2]    #filters to connected peers
+        peers=peers[peers['status']=='OK']    #filters to connected peers
         connectedpeers=len(peers)
         peerheight=peers['height'].mode()[0]    #calculates the mode height from connected peers
+        print(peerheight)
         consensus=round(len(peers[abs(peers['height']-peerheight)<=tol])/total*100,2) #calculates consensus from peer height
+        backupheights['Peers: '+str(connectedpeers)]='{:,.0f}'.format(peerheight)
+        backupheights['Consensus']='{:.1f}%'.format(consensus)
     except:
         connectedpeers='not available'
         peerheight='not available'
         consensus='not available'
+        backupheights['Peers: '+connectedpeers]=peerheight
+        backupheights['Consensus']=consensus
+    backupheights=pd.DataFrame.from_dict(backupheights,orient='index')
+    backupheights.columns = ['Height']
     return connectedpeers,peerheight,consensus,backupheights
 
 def getheight(url):
@@ -149,8 +154,7 @@ def getallchannelids(channelids,userids,apitoken):
 '''__________NOTIFICATION FUNCTIONS___________'''
 
 def processdelegates(delegatesnew,delegates):
-    #compares the current and previous delegate block counts to track consecutive missed/produced blocks
-    delegatesold=delegates
+    """compares the current and previous delegate block counts to track consecutive missed/produced blocks"""
     delegatesnew['missedblocksmsg']=0
     if delegates is None:
         #if no previous delegate block counts are available, start missed/produced block counters at 0
@@ -161,18 +165,17 @@ def processdelegates(delegatesnew,delegates):
         delegates.rename(columns={'missedblocks': 'missedold','producedblocks':'producedold','missedblocksmsg':'msgold'}, inplace=True)
         delegates=delegates[['username','missedold','producedold','newmissedblocks','newproducedblocks','msgold']]
         delegatesnew=pd.merge(delegatesnew,delegates,how='left',on='username')
-        delegatesnew['missedblocksmsg']=delegatesnew['missedblocksmsg']+delegatesnew['msgold']
-        delegatesnew['newmissedblocks']=delegatesnew['newmissedblocks']+delegatesnew['missedblocks']-delegatesnew['missedold']
+        delegatesnew['missedblocksmsg']=np.maximum(0,delegatesnew['missedblocksmsg']+delegatesnew['msgold'])
+        delegatesnew['newmissedblocks']=np.maximum(0,delegatesnew['newmissedblocks']+delegatesnew['missedblocks']-delegatesnew['missedold'])
         #resets consecutive produced block counter to 0 if a delegate misses a block
         delegatesnew.loc[delegatesnew['missedblocks']-delegatesnew['missedold']>0, ['newproducedblocks']] = 0
-        delegatesnew['newproducedblocks']=delegatesnew['newproducedblocks']+delegatesnew['producedblocks']-delegatesnew['producedold']
+        delegatesnew['newproducedblocks']=np.maximum(0,delegatesnew['newproducedblocks']+delegatesnew['producedblocks']-delegatesnew['producedold'])
         #resets consecutive missed block counter to 0 if a delegate produces a block
         delegatesnew.loc[delegatesnew['producedblocks']-delegatesnew['producedold']>0, ['newmissedblocks','missedblocksmsg']] = 0
         #resets all counters to 0 if a delegate begins forging
         delegatesnew.loc[delegatesnew['newmissedblocks'].isnull(), ['newmissedblocks','missedblocksmsg','newproducedblocks']] = 0
+        #drops temporary columns
         delegatesnew=delegatesnew.drop(['missedold','producedold','msgold'],axis=1)
-        if len(delegatesnew[delegatesnew['newproducedblocks']<0].index)>0:
-            delegatesnew=delegatesold
         return delegatesnew
 
 def checknames(name):
@@ -204,6 +207,7 @@ def modifymissedblockmsglist(missedblockmsglist,slacknames,userlist):
     newmissedblockmsglist=[]
     for i in missedblockmsglist:
         delegate=i["username"]
+        display=''
         name=''
         names=checknames(delegate)
         for j in slacknames:
@@ -231,17 +235,17 @@ def makemissedblockmsg(missedblockmsglist,blockinterval=0,includeprevious=False)
             if message!="":
                 message=message+"\n"
             if i["missedblocksmsg"]>blockinterval:
-                message=message+i["username"] +"still red :alert:"
+                message=message+i["username"] +"still red :small_red_triangle_down:"
             elif i["missedblocksmsg"]>1:
-                message=message+i["username"] +"red :alert:"
+                message=message+i["username"] +"red :small_red_triangle_down:"
             else:
                 message=message+i["username"] +"yellow :warning:"
     else:
-        redmessage=":alert: "
+        redmessage=":small_red_triangle_down: "
         yellowmessage=":warning: "
         for i in missedblockmsglist:
             if i["missedblocksmsg"]>1:
-                if redmessage != ":alert: ":
+                if redmessage != ":small_red_triangle_down: ":
                     redmessage+=", "+i["username"]
                 else:
                     redmessage+=i["username"]
@@ -250,9 +254,9 @@ def makemissedblockmsg(missedblockmsglist,blockinterval=0,includeprevious=False)
                     yellowmessage+=", "+i["username"]
                 else:
                     yellowmessage+=i["username"]
-        redmessage+=":alert:"
+        redmessage+=":small_red_triangle_down:"
         yellowmessage+=":warning:"
-        if redmessage != ":alert: :alert:":
+        if redmessage != ":small_red_triangle_down: :small_red_triangle_down:":
             message=redmessage
             if yellowmessage != ":warning: :warning:":
                 message+="\n"+yellowmessage
@@ -262,55 +266,22 @@ def makemissedblockmsg(missedblockmsglist,blockinterval=0,includeprevious=False)
 
 '''__________RESPONSE FUNCTIONS___________'''
 
-def getpools(file):
-    pfile = open(file, 'r')
-    pools = pfile.read()
-    pfile.close
-    pools = pools.replace('`','').lower()
-    pools = max(pools.split('*'), key=len).split(';')
-    pools = pd.DataFrame(pools,columns=['string'])
-    pools['string'].str.lower()
-    pools = pools['string'].str.extractall(r'^[*]?\s*\-*\s*(?P<delegate>[\w.-]+)?\,*\s*(?P<delegate2>[\w]+)?\,*\s*(?P<delegate3>[\w]+)?\,*\s*(?P<delegate4>[\w]+)?\,*\s*(?P<delegate5>[\w]+)?\,*\s<*(?P<website>[\w./:-]+)?>*\s*\(\`*[0-9x]*?(?P<percentage>[0-9.]+)\%\s*\-*(?P<listed_frequency>\w+)*\`*\,*\s*(?:min)?\.*\s*(?:payout)?\s*(?P<min_payout>[0-9.]+)*\s*(?P<coin>\w+)*?\s*(?:payout)?\`*[\w ]*\).*?$')
-    dropcols=['coin']
-    pools=pools.drop(dropcols,axis=1)
-    pools.loc[pools['listed_frequency']=='c', ['listed_frequency']] = np.nan
-    pools.loc[pools['listed_frequency']=='2d', ['listed_frequency']] = 2
-    pools.loc[pools['listed_frequency']=='w', ['listed_frequency']] = 7
-    pools.loc[pools['listed_frequency']=='d', ['listed_frequency']] = 1
-    pools['listed_frequency']=pd.to_numeric(pools['listed_frequency'])
-    pools['min_payout']=pd.to_numeric(pools['min_payout'])
-    pools.rename(columns={'percentage': 'listed % share'}, inplace=True)
-    pools=pd.melt(pools, id_vars=['listed % share','listed_frequency','min_payout','website'], var_name='delegatenumber', value_name='delegate')
-    del pools['delegatenumber']
-    pools=pools.loc[pools['delegate'].notnull()]
-    pools=pools.reset_index(drop=True)
-    pools['listed % share']=pd.to_numeric(pools['listed % share'])
-    pools=pools.sort_values(by='listed % share',ascending=False)
-    return pools
+def printdelegates(delegates,rank,limit):
+    """outputs the delegates list in a friendly format"""
+    delegates=delegates.loc[(delegates['rate']>=rank-limit)&(delegates['rate']<=rank+limit)]
+    delegates['vote'] = delegates['vote']/100000000
+    delegates['voteweight'] = (delegates['vote']/1000).map('{:,.3f}'.format).astype(str) + 'K'
+    delegates['productivity'] = delegates['productivity'].map('{:,.2f}%'.format)
+    delegates['approval'] = delegates['approval'].map('{:,.2f}%'.format)
+    ind=(delegates['rate'].values.tolist()).index(rank)
+    delegates=insertblankrow(delegates,ind+1)
+    cleandelegates=delegates[['rate','username','approval','voteweight']].to_string(index=False)
+    return cleandelegates
 
-def getpoolstats(pools,delegates,numdelegates,blockrewards,blockspermin,balance=10000):
-    delegates=delegates[['username','rank','vote','address']]
-    totalrewardsperday=blockrewards*blockspermin*60*24/numdelegates
-    poolstats=pd.merge(pools,delegates,how='left',left_on='delegate',right_on='username')
-    poolstats['rewards/day']=((balance/poolstats['vote'])*totalrewardsperday*(poolstats['listed % share']/100)).round(2)
-    del poolstats['username']
-    poolstats=poolstats[poolstats['vote']>=0]
-    poolstats['rank']=poolstats['rank'].astype('int64')
-    poolstats['listed % share']=poolstats['listed % share'].astype('int64')
-    del poolstats['vote']
-    cols = list(poolstats)
-    cols.insert(0, cols.pop(cols.index('rank')))
-    poolstats = poolstats.ix[:, cols]
-    poolstats=poolstats.sort_values(by='rewards/day',ascending=False)
-    return poolstats
-
-def printforgingpools(pools):
-    cleanpools='*`Sharing Pools (Forging)`* -'
-    pool=pools.sort_values(by='rank')
-    pools['delegate']='_#'+pools['rank'].astype(str)+'_-*'+pools['delegate']+'*'
-    pools=pools.groupby(['listed % share'])['delegate'].apply(', '.join).reset_index()
-    pools=pools.sort_values(by='listed % share',ascending=False)
-    for index,row in pools.iterrows():
-        cleanpools+=' `'+str(row['listed % share'])+'%'+'` '+row['delegate']
-    cleanpools+=' *_`We in no way endorse any of the pools shown here and only provide the list as a help to the community. The list only reflects the information we have been provided, we cannot police the pools and voters should do their due diligence before voting`_*'
-    return cleanpools
+def insertblankrow(df,ind):
+    """inserts a blank row into a dataframe at the specified index"""
+    cols=list(df.columns.values)
+    blank=pd.Series([''],index=cols)
+    result=df.iloc[:ind].append(blank,ind)
+    result=result.append(df.iloc[ind:],ind)
+    return result
